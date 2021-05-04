f10625e79b00ce073b80debcb0f530018488a101
jecmenicam <jecmenicam@ae.com>
2021-05-05 01:12:10 +0200

    Added support for distributed tracing
    Added Sleuth to allow tracking of spans and traces and addded
    Zipkin for visualisation of captured spans.

--- inventory/src/main/java/gridu/milanjecmenica/inventory/service/GridUInventoryService.java
+++ inventory/src/main/java/gridu/milanjecmenica/inventory/service/GridUInventoryService.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 
 import gridu.milanjecmenica.inventory.model.Inventory;
 import gridu.milanjecmenica.inventory.repository.InventoryRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class GridUInventoryService implements InventoryService {
 
     public final static Inventory stub = new Inventory();
@@ -16,6 +18,7 @@ public class GridUInventoryService implements InventoryService {
 
     @Override
     public Inventory getInventory(String id) {
+        log.info("GridUInventoryService :: getInventory - Success!");
         return this.inventoryRepository.findById(id).orElse(stub);
     }
 
