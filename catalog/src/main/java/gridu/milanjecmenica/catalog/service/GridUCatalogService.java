9f7023227b0227ffad79d984023da26c8eff7b33
jecmenicam <jecmenicam@ae.com>
2021-05-06 00:22:55 +0200

    Added circuit breaker support with resilience4j
    Added default circuit breaker behaviour for every service and
    behaviour for catalog service.

--- catalog/src/main/java/gridu/milanjecmenica/catalog/service/GridUCatalogService.java
+++ catalog/src/main/java/gridu/milanjecmenica/catalog/service/GridUCatalogService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import gridu.milanjecmenica.catalog.model.Product;
 import gridu.milanjecmenica.catalog.repository.CatalogRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @Service
 public class GridUCatalogService implements CatalogService {
@@ -21,15 +23,29 @@ public class GridUCatalogService implements CatalogService {
     }
 
     @Override
+    @CircuitBreaker(name = "default", fallbackMethod="defaultProduct")
+    @Retry(name = "default", fallbackMethod="defaultProduct")
     public Product getProduct(String id) {
-        Product product = this.catalogRepository.findById(id).orElse(productStub);
-        return product;
+        throw new NullPointerException();
+        // Product product = this.catalogRepository.findById(id).orElse(productStub);
+        // return product;
     }
 
     @Override
+    @CircuitBreaker(name = "default")
+    @Retry(name = "default")
     public List<Product> getProducts(String sku) {
         List<Product> productList = this.catalogRepository.findBySku(sku);
         return productList;
     }
     
+    public Product defaultProduct(String id, Throwable t) {
+        Product product = new Product();
+        product.setId("test");
+        product.setCategory("test");
+        product.setColor("test");
+        product.setName("test");
+        product.setSku("test");
+        return product;
+    }
 }
